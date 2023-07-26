3ca37fe6fc6055ca6481aaf01b43981ba6811c9d
Oskar <assawin.h@gmail.com>
2023-07-26 14:56:38 +0200

    Added ncat which is basically just cat with less features. Its pretty fast though. I used fwrite and after a few tried it turned out to be super fast!

---
+++ ncat.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define BUF_SZ 2048
+
+int main (int argc, char *argv[]) {
+
+if (argc < 2 || argc > 2) {
+    fprintf(stderr, "%s: Please provide 1 file.\n", argv[0]);
+    return EXIT_FAILURE;
+}
+
+int c;
+FILE *file;
+file = fopen(argv[1],"rb");
+
+if (file == NULL) {
+    fprintf(stderr, "Cannot open file.\n");
+    return EXIT_FAILURE;
+}
+
+char buffer[BUF_SZ];
+size_t bytes_read;
+while ((bytes_read = fread(buffer, 1, BUF_SZ, file)) > 0) {
+    fwrite(buffer, 1, bytesRead, stdout);
+}
+
+fclose(file);
+
+return EXIT_SUCCESS;
+
+}
